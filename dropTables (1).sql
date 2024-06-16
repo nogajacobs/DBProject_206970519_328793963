e6fe37237b748f14c9b6b91b4d980e163fc429cc
Adinakallus <73253528+Adinakallus@users.noreply.github.com>
2024-06-16 08:57:51 +0300

    Add files via upload

---
+++ dropTables (1).sql
@@ -0,0 +1,7 @@
+DROP TABLE  Registration;
+DROP TABLE  Teacher;
+DROP TABLE Daycare_Activities;
+DROP TABLE  Activities;
+DROP TABLE  Daycare;
+DROP TABLE Catering;
+DROP TABLE Child;
---
+++ selectAll (1).sql
@@ -0,0 +1,7 @@
+SELECT * FROM Child;
+SELECT * FROM Catering;
+SELECT * FROM Daycare;
+SELECT * FROM Activities;
+SELECT * FROM Daycare_Activities;
+SELECT * FROM Teacher;
+SELECT * FROM Registration;

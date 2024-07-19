d3cc7a65cd3d953fa440650742ad3039b5fc050a
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-19 13:19:30 +0300

    Create InsertCatering.sql

---
+++ levelOne/InsertTables/InsertCatering.sql
@@ -0,0 +1,84 @@
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0541234567', 'rabanut', 'Tel Aviv', 1, 'Delicious Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0522345678', 'rabanut mehadrin', 'Jerusalem', 2, 'Jerusalem Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0503456789', 'eida charedit', 'Haifa', 3, 'Haifa Delights');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0534567890', 'rubin', 'Beer Sheva', 4, 'Negev Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0545678901', 'bet yosef', 'Eilat', 5, 'Eilat Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0526789012', 'mehuderet', 'Rishon LeZion', 6, 'Rishon Tastes');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0507890123', 'rabanut', 'Netanya', 7, 'Netanya Feasts');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0538901234', 'rabanut mehadrin', 'Petah Tikva', 8, 'Petah Delicacies');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0549012345', 'eida charedit', 'Ashdod', 9, 'Ashdod Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0520123456', 'rubin', 'Holon', 10, 'Holon Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0501234567', 'bet yosef', 'Bnei Brak', 11, 'Bnei Brak Tastes');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0532345678', 'mehuderet', 'Bat Yam', 12, 'Bat Yam Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0543456789', 'rabanut', 'Herzliya', 13, 'Herzliya Delights');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0524567890', 'rabanut mehadrin', 'Kfar Saba', 14, 'Kfar Saba Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0505678901', 'eida charedit', 'Rehovot', 15, 'Rehovot Feasts');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0536789012', 'rubin', 'Ashkelon', 16, 'Ashkelon Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0547890123', 'bet yosef', 'Raanana', 17, 'Raanana Tastes');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0528901234', 'mehuderet', 'Modiin', 18, 'Modiin Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0509012345', 'rabanut', 'Nahariya', 19, 'Nahariya Delights');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0530123456', 'rabanut mehadrin', 'Afula', 20, 'Afula Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0541234567', 'eida charedit', 'Tiberias', 21, 'Tiberias Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0522345678', 'rubin', 'Yavne', 22, 'Yavne Feasts');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0503456789', 'bet yosef', 'Karmiel', 23, 'Karmiel Delights');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0534567890', 'mehuderet', 'Beit Shemesh', 24, 'Beit Shemesh Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0545678901', 'rabanut', 'Kiryat Gat', 25, 'Kiryat Gat Tastes');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0526789012', 'rabanut mehadrin', 'Hadera', 26, 'Hadera Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0507890123', 'eida charedit', 'Tzfat', 27, 'Tzfat Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0538901234', 'rubin', 'Arad', 28, 'Arad Feasts');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0549012345', 'bet yosef', 'Dimona', 29, 'Dimona Delights');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0520123456', 'mehuderet', 'Maale Adumim', 30, 'Maale Adumim Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0501234567', 'rabanut', 'Zichron Yaakov', 31, 'Zichron Yaakov Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0532345678', 'rabanut mehadrin', 'Rosh HaAyin', 32, 'Rosh HaAyin Feasts');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0543456789', 'eida charedit', 'Kiryat Shmona', 33, 'Kiryat Shmona Delights');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0524567890', 'rubin', 'Sderot', 34, 'Sderot Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0505678901', 'bet yosef', 'Ness Ziona', 35, 'Ness Ziona Tastes');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0536789012', 'mehuderet', 'Kiryat Yam', 36, 'Kiryat Yam Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0547890123', 'rabanut', 'Or Yehuda', 37, 'Or Yehuda Feasts');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0528901234', 'rabanut mehadrin', 'Migdal HaEmek', 38, 'Migdal HaEmek Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0509012345', 'eida charedit', 'Ramat HaSharon', 39, 'Ramat HaSharon Delights');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0530123456', 'rubin', 'Kiryat Malakhi', 40, 'Kiryat Malakhi Catering');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0541234567', 'bet yosef', 'Ariel', 41, 'Ariel Feasts');
+insert into CATERING (phone_number, kashrut, location, c_id, catering_name)
+values ('0522345678', 'mehuderet', 'Sakhnin', 42, 'Sakhnin Catering');

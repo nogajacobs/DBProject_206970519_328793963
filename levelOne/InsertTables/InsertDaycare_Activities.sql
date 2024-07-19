7e290c1b8e2518b11243948517f4a708781b1fac
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-19 13:24:26 +0300

    Create InsertDaycare_Activities.sql

---
+++ levelOne/InsertTables/InsertDaycare_Activities.sql
@@ -0,0 +1,36 @@
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (1, '0541234567');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (2, '0541234567');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (3, '0542345678');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (4, '0542345678');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (5, '0543456789');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (6, '0543456789');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (7, '0544567890');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (8, '0544567890');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (9, '0545678901');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (10, '0545678901');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (11, '0546789012');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (12, '0546789012');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (13, '0547890123');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (14, '0548901234');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (15, '0547890123');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (16, '0548901234');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (17, '0549012345');
+insert into DAYCARE_ACTIVITIES (d_id, contact_number)
+values (18, '0549012345');

e437dcada990816afc88c63091060c2a953cdaec
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-19 13:18:54 +0300

    Create InsertActivities.sql

---
+++ levelOne/InsertTables/InsertActivities.sql
@@ -0,0 +1,100 @@
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0541234567', 'Art', 'Art in Love');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0542345678', 'Music', 'Musical Wonders');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0543456789', 'Dance', 'Dancing Stars');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0544567890', 'Sports', 'Sport Champions');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0545678901', 'Swimming', 'Swim Masters');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0546789012', 'Drama', 'Drama Queens');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0547890123', 'Science', 'Science Explorers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0548901234', 'Math', 'Math Geniuses');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0549012345', 'Cooking', 'Culinary Artists');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0550123456', 'Coding', 'Code Breakers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0551234567', 'Robotics', 'Robo Tech');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0552345678', 'Gardening', 'Green Thumbs');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0553456789', 'Chess', 'Chess Masters');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0554567890', 'Painting', 'Color Palette');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0555678901', 'Crafting', 'Craft Wizards');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0556789012', 'Puzzles', 'Puzzle Solvers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0557890123', 'Storytelling', 'Story Time');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0558901234', 'Language', 'Language Lovers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0559012345', 'Photography', 'Photo Snappers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0560123456', 'Debate', 'Debate Club');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0561234567', 'Yoga', 'Yoga Harmony');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0562345678', 'Zumba', 'Zumba Dancers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0563456789', 'Karate', 'Karate Kids');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0564567890', 'Ballet', 'Ballet Beauties');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0565678901', 'Pilates', 'Pilates Pros');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0566789012', 'Boxing', 'Boxing Champs');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0567890123', 'Soccer', 'Soccer Stars');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0568901234', 'Basketball', 'Basketball Heroes');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0569012345', 'Volleyball', 'Volleyball Victors');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0570123456', 'Handball', 'Handball Heroes');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0571234567', 'Hockey', 'Hockey Players');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0572345678', 'Tennis', 'Tennis Aces');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0573456789', 'Table Tennis', 'Ping Pong Masters');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0574567890', 'Running', 'Run Fast');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0575678901', 'Jump Rope', 'Jump Ropers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0576789012', 'Ice Skating', 'Ice Skaters');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0577890123', 'Skiing', 'Ski Experts');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0578901234', 'Skateboarding', 'Skateboard Riders');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0579012345', 'Surfing', 'Surf Riders');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0580123456', 'Sailing', 'Sailors Club');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0581234567', 'Mountain Biking', 'Mountain Bikers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0582345678', 'Rock Climbing', 'Rock Climbers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0583456789', 'Hiking', 'Hiking Crew');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0584567890', 'Fishing', 'Fishing Friends');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0585678901', 'Horse Riding', 'Horse Riders');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0586789012', 'Bird Watching', 'Bird Watchers');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0587890123', 'Camping', 'Campers Club');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0588901234', 'Archery', 'Archery Aces');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0589012345', 'Fencing', 'Fencing Warriors');
+insert into ACTIVITIES (contact_number, activity_type, operator_name)
+values ('0590123456', 'Cricket', 'Cricket Champs');

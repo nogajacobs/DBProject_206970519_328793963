ea9292e15d0bedc37f673a83d6877405061bd400
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-19 13:25:02 +0300

    Create InsertRegistration.sql

---
+++ levelOne/InsertTables/InsertRegistration.sql
@@ -0,0 +1,100 @@
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1000, 1, to_date('01-06-2023', 'dd-mm-yyyy'), 4, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1200, 2, to_date('15-07-2023', 'dd-mm-yyyy'), 5, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1100, 3, to_date('20-08-2023', 'dd-mm-yyyy'), 6, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1500, 4, to_date('10-09-2023', 'dd-mm-yyyy'), 4, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1300, 5, to_date('05-10-2023', 'dd-mm-yyyy'), 5, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1400, 6, to_date('20-11-2023', 'dd-mm-yyyy'), 6, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1250, 7, to_date('15-12-2023', 'dd-mm-yyyy'), 4, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1150, 8, to_date('10-01-2024', 'dd-mm-yyyy'), 5, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1350, 9, to_date('05-02-2024', 'dd-mm-yyyy'), 6, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1550, 10, to_date('01-03-2024', 'dd-mm-yyyy'), 4, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1450, 11, to_date('20-04-2024', 'dd-mm-yyyy'), 5, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1650, 12, to_date('15-05-2024', 'dd-mm-yyyy'), 6, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1200, 13, to_date('10-06-2024', 'dd-mm-yyyy'), 4, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1100, 14, to_date('05-07-2024', 'dd-mm-yyyy'), 5, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1000, 15, to_date('01-08-2024', 'dd-mm-yyyy'), 6, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1250, 16, to_date('20-09-2024', 'dd-mm-yyyy'), 4, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1300, 17, to_date('15-10-2024', 'dd-mm-yyyy'), 5, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1400, 18, to_date('10-11-2024', 'dd-mm-yyyy'), 6, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1450, 19, to_date('05-12-2024', 'dd-mm-yyyy'), 4, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1550, 20, to_date('01-01-2025', 'dd-mm-yyyy'), 5, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1650, 21, to_date('20-02-2025', 'dd-mm-yyyy'), 6, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1350, 22, to_date('15-03-2025', 'dd-mm-yyyy'), 4, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1200, 23, to_date('10-04-2025', 'dd-mm-yyyy'), 5, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1100, 24, to_date('05-05-2025', 'dd-mm-yyyy'), 6, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1000, 25, to_date('01-06-2025', 'dd-mm-yyyy'), 4, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1250, 26, to_date('20-07-2025', 'dd-mm-yyyy'), 5, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1150, 27, to_date('15-08-2025', 'dd-mm-yyyy'), 6, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1350, 28, to_date('10-09-2025', 'dd-mm-yyyy'), 4, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1550, 29, to_date('05-10-2025', 'dd-mm-yyyy'), 5, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1650, 30, to_date('01-11-2025', 'dd-mm-yyyy'), 6, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1200, 31, to_date('20-12-2025', 'dd-mm-yyyy'), 4, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1100, 32, to_date('15-01-2026', 'dd-mm-yyyy'), 5, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1000, 33, to_date('10-02-2026', 'dd-mm-yyyy'), 6, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1250, 34, to_date('05-03-2026', 'dd-mm-yyyy'), 4, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1300, 35, to_date('01-04-2026', 'dd-mm-yyyy'), 5, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1400, 36, to_date('20-05-2026', 'dd-mm-yyyy'), 6, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1450, 37, to_date('15-06-2026', 'dd-mm-yyyy'), 4, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1550, 38, to_date('10-07-2026', 'dd-mm-yyyy'), 5, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1650, 39, to_date('05-08-2026', 'dd-mm-yyyy'), 6, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1350, 40, to_date('01-09-2026', 'dd-mm-yyyy'), 4, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1200, 41, to_date('20-10-2026', 'dd-mm-yyyy'), 5, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1100, 42, to_date('15-11-2026', 'dd-mm-yyyy'), 6, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1000, 43, to_date('10-12-2026', 'dd-mm-yyyy'), 4, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1250, 44, to_date('05-01-2027', 'dd-mm-yyyy'), 5, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1150, 45, to_date('01-02-2027', 'dd-mm-yyyy'), 6, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1350, 46, to_date('20-03-2027', 'dd-mm-yyyy'), 4, 1);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1550, 47, to_date('15-04-2027', 'dd-mm-yyyy'), 5, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1650, 48, to_date('10-05-2027', 'dd-mm-yyyy'), 6, 3);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1200, 49, to_date('05-06-2027', 'dd-mm-yyyy'), 4, 2);
+insert into REGISTRATION (price, registration_id, registration_date, d_id, child_id)
+values (1100, 50, to_date('01-07-2027', 'dd-mm-yyyy'), 5, 3);

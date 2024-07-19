ed0941776a00e40ec4d99994d965f22829b1a009
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-19 12:54:53 +0300

    Update and rename createTables (1).sql to Catering.sql

---
+++ levelOne/CreateTables/Catering.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Catering
+(
+  Phone_Number INT NOT NULL,
+  Kashrut INT NOT NULL,
+  Location INT NOT NULL,
+  C_ID INT NOT NULL,
+  Catering_Name INT NOT NULL,
+  PRIMARY KEY (C_ID)
+);

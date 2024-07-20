5dfa8de4e6731362efda1a26c88a946348a8b477
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-21 01:07:17 +0300

    Update QueriesUpdateOne.sql

--- levelTwo/Queries/UpdateQueries/QueriesUpdateOne.sql
+++ levelTwo/Queries/UpdateQueries/QueriesUpdateOne.sql
@@ -1,8 +1,9 @@
--- Query 1: Increase the price of registration for all registrations in a specific daycare by 10%
+-- שאילתה 1: העלאת מחיר ההרשמה לכל ההרשמות בגן ילדים ספציפי ב-10%
 UPDATE Registration
-SET Price = Price * 1.10
+SET Price = Price * 1.10 -- עדכון מחיר ההרשמה להגדלה של 10%
 WHERE D_ID = (
-	SELECT D_ID
-	FROM Daycare 
-	WHERE Daycare_Name = 'Daycare_B'
+    -- תת-שאילתה למציאת מזהה גן הילדים לפי שם הגן
+    SELECT D_ID
+    FROM Daycare 
+    WHERE Daycare_Name = 'Daycare_B' -- שם גן הילדים שבו יבוצע עדכון המחיר
 );

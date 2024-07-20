638a5854a673030b4c7fc017223f597e3663ad40
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-21 01:12:07 +0300

    Update QuerieParamsThree.sql

--- levelTwo/Queries/ParamsQueries/QuerieParamsThree.sql
+++ levelTwo/Queries/ParamsQueries/QuerieParamsThree.sql
@@ -1,18 +1,19 @@
---2.
---Parents can use this query to find daycares in specific locations that offer catering services with 
---a particular kashrut level and belong to a specific sector.
+-- 2.
+-- הורים יכולים להשתמש בשאילתה זו כדי למצוא גני ילדים במיקומים ספציפיים המציעים שירותי הסעדה
+-- עם רמת כשרות מסוימת ושייכים למגזר מסוים.
 
--- Parameters: :p_locations (List of Locations), :p_kashrut (Kashrut Level), :p_sector (Sector)
+-- פרמטרים: :p_locations (רשימת מיקומים), :p_kashrut (רמת כשרות), :p_sector (מגזר)
+
 SELECT 
-    d.Daycare_Name,
-    d.Location,
-    cat.Catering_Name,
-    cat.Kashrut
+    d.Daycare_Name, -- שם גן הילדים
+    d.Location, -- מיקום גן הילדים
+    cat.Catering_Name, -- שם שירות ההסעדה
+    cat.Kashrut -- רמת הכשרות של שירות ההסעדה
 FROM 
     Daycare d
 JOIN 
-    Catering cat ON d.C_ID = cat.C_ID
+    Catering cat ON d.C_ID = cat.C_ID -- הצטרפות לטבלת Catering לפי מזהה שירות ההסעדה
 WHERE 
-    d.Location IN (:p_locations)
-    AND cat.Kashrut = :p_kashrut
-    AND d.Sector = :p_sector;
+    d.Location IN (:p_locations) -- סינון לפי רשימת מיקומים (פרמטר)
+    AND cat.Kashrut = :p_kashrut -- סינון לפי רמת הכשרות של שירות ההסעדה (פרמטר)
+    AND d.Sector = :p_sector; -- סינון לפי מגזר (פרמטר)

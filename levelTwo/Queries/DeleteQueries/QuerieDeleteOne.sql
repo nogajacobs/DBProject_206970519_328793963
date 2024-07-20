59bd71c921429bcbe9beac46cb87c23126e3b022
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-21 01:14:53 +0300

    Update QuerieDeleteOne.sql

--- levelTwo/Queries/DeleteQueries/QuerieDeleteOne.sql
+++ levelTwo/Queries/DeleteQueries/QuerieDeleteOne.sql
@@ -1,12 +1,13 @@
---The daycare system needs to remove registration records for children who registered
--- more than three years ago during the summer months (June, July, and August).
- --This helps in maintaining a cleaner and more relevant database, focusing on current
--- and recent registrations.
+-- מערכת גני הילדים צריכה למחוק רישומים עבור ילדים שנרשמו
+-- לפני יותר משלוש שנים במהלך חודשי הקיץ (יוני, יולי ואוגוסט).
+-- זה מסייע בשמירה על בסיס נתונים נקי ורלוונטי יותר, תוך מיקוד ברשומות נוכחיות וחדשות.
+
 DELETE FROM Registration
 WHERE 
-    EXTRACT(YEAR FROM Date) < EXTRACT(YEAR FROM CURRENT_DATE) - 3
-    AND EXTRACT(MONTH FROM Date) IN (6, 7, 8)
+    EXTRACT(YEAR FROM Date) < EXTRACT(YEAR FROM CURRENT_DATE) - 3 -- מחיקת רישומים שהיו לפני יותר משלוש שנים
+    AND EXTRACT(MONTH FROM Date) IN (6, 7, 8) -- מחיקת רישומים שהתבצעו בחודשי הקיץ (יוני, יולי ואוגוסט)
     AND Child_ID NOT IN (
+        -- תת-שאילתה למציאת Child_IDs שלא נרשמו לפעילויות גן ילדים
         SELECT DISTINCT Child_ID
         FROM Daycare_Activities da
         JOIN Registration r ON da.D_ID = r.D_ID

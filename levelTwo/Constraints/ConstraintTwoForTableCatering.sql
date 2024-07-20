f1446d166ec941ce5ac0b5e5be41577378312a6c
nogajacobs <80648050+nogajacobs@users.noreply.github.com>
2024-07-21 01:17:21 +0300

    Update ConstraintTwoForTableCatering.sql

--- levelTwo/Constraints/ConstraintTwoForTableCatering.sql
+++ levelTwo/Constraints/ConstraintTwoForTableCatering.sql
@@ -1,4 +1,4 @@
---Unique Constraint on Catering Name
---Ensure that each catering company has a unique name to prevent duplication.
+--הגבלה ייחודית על שם הקייטרינג
+--וודאו שלכל חברת קייטרינג יש שם ייחודי למניעת כפילות.
 ALTER TABLE Catering
 ADD CONSTRAINT unique_catering_name UNIQUE (Catering_Name);
